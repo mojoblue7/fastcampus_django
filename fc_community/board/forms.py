f54f271558fa69fea9cd5d62f8dbece03922b3e7
mojoblue7 <lsh18542@gmail.com>
2019-07-20 22:38:48 +0900

    add board_list and board_write

---
+++ fc_community/board/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class BoardForm(forms.Form):
+    title = forms.CharField(error_messages={
+        'required': '제목을 입력해주세요.'
+    }, max_length=1282, label="제목")
+    contents = forms.CharField(error_messages={
+        'required': '내용을 입력해주세요'
+    }, widget=forms.Textarea, label="내용")
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
-
+from .forms import LoginForm
 # Create your views here.
 
 
@@ -17,24 +17,22 @@ def home(request):
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
 
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                request.session['user'] = fcuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호가 틀렸습니다.'
+    return render(request, 'login.html', {'form': form})
 
-        return render(request, 'login.html', res_data)
+
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+
+    return redirect('/')
 
 
 def register(request):
