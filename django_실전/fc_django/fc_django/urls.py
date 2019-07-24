d0a2b389e44f59f8e2aadfe012d3977e94cc0395
mojoblue7 <lsh18542@gmail.com>
2019-07-24 15:57:27 +0900

    add register forms

--- django_실전/fc_django/fc_django/urls.py
+++ django_실전/fc_django/fc_django/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from fcuser.views import index
+from fcuser.views import index, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('register/', RegisterView.as_view())
 ]
