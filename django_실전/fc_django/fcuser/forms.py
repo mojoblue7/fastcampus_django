4c02a1d06dd2f1b1a98562193dd75e4632f2fe8f
mojoblue7 <lsh18542@gmail.com>
2019-07-26 23:26:11 +0900

    add order page and templates

--- django_실전/fc_django/fcuser/forms.py
+++ django_실전/fc_django/fcuser/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.hashers import check_password, make_password
+from .models import Fcuser
 
 class RegisterForm(forms.Form):
     email = forms.EmailField(
@@ -23,6 +24,7 @@ class RegisterForm(forms.Form):
 
     def clean(self):
         cleaned_data = super().clean()
+        email = cleaned_data.get('email')
         password = cleaned_data.get('password')
         re_password = cleaned_data.get('re_password')
 
@@ -30,3 +32,40 @@ class RegisterForm(forms.Form):
             if password != re_password:
                 self.add_error('password', '비밀번호가 서로 다릅니다.')
                 self.add_error('re_password', '비밀번호가 서로 다릅니다.')
+            else:
+                fcuser = Fcuser(
+                    email=email,
+                    password=make_password(password)
+                )
+                fcuser.save()
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64, label='이메일'
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label='비밀번호'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                fcuser = Fcuser.objects.get(email=email)
+            except Fcuser.DoesNotExist:
+                self.add_error('email', '아이디가 없습니다')
+                return
+
+            if not check_password(password, fcuser.password):
+                self.add_error('password', '비밀번호를 틀렸습니다')
+            else:
+                self.email = fcuser.email
